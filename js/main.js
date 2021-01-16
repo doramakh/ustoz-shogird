046498059541b1839a2cf9668f11ea64e19d3cd8
00009054 <00009054.wiut@gmail.com>
2021-01-17 00:21:39 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $_ = (selector, node = document) => {
+  return node.querySelector(selector);
+};
+
+var $$_ = (selector, node = document) => {
+  return node.querySelectorAll(selector);
+};
+
+var createElement = (element, elementClass, text) => {
+  var newElement = document.createElement(element);
+
+  if (elementClass) {
+    newElement.setAttribute('class', elementClass);
+  };
+  
+  if (text) {
+    newElement.textContent = text;
+  };
+};
+

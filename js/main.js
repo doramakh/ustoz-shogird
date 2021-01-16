652466c1f92dd3303e5eae96421cfa7f5b1f4074
doramakh <anvarovna28@gmail.com>
2021-01-17 01:57:27 +0500

    announce html elements in js

--- js/main.js
+++ js/main.js
@@ -17,3 +17,20 @@ var createElement = (element, elementClass, text) => {
     newElement.textContent = text;
   };
 };
+
+var elNewAnnouncementBtn = $_('.new-announcement-btn');
+var elAnnouncementForm = $_('.announcement-form');
+if (elAnnouncementForm) {
+  var elTitleInput = $_('.title-input', elAnnouncementForm);
+  var elCompanyInput = $_('.company-input', elAnnouncementForm);
+  var elTechInput = $_('.tech-input', elAnnouncementForm);
+  var elTelegramInput = $_('.telegram-input', elAnnouncementForm);
+  var elPhoneNumberInput = $_('.phone-number-input', elAnnouncementForm);
+  var elNameInput = $_('.name-input', elAnnouncementForm);
+  var elLocationSelect = $_('.location-select', elAnnouncementForm);
+  var elWorktimeSelect = $_('.worktime-select', elAnnouncementForm);
+  var elSalaryInput = $_('.salary-input', elAnnouncementForm);
+  var elInfoTextarea = $_('.info-textarea', elAnnouncementForm);
+};
+var elAnnouncementList = $_('.announcement-list');
+var elAnnouncementTemplate = $_('.announcement-template');
